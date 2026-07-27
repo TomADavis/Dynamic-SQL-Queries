c546b27b8dd06982a377bd61cea13437afa88cc4
TomADavis <tomanthonydavis@gmail.com>
2026-07-27 16:38:55 +0100

    added a mapping into the config file for dates -> resective field in that db

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ from sqlalchemy import create_engine
 instacart = {
     "engine": create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/instacart"),
     "sql_file": "orders.sql",
+    "columns": {"date_range": "order_date"},
 }
 
 films = {
     "engine": create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/films_db"),
-    "sql_file": "films_db.sql"
+    "sql_file": "films_db.sql",
+    "columns": {"date_range": "release_date"},
 }
 
